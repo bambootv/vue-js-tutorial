ab182e4bb2d8b4498a218ea09d26e59a1fdea0f6
Hoan Ky <hoan.nguyen@luxstay.com>
2020-02-14 17:57:51 +0700

    Chapter 1

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/">Home</router-link>
+    <router-link to="/">Home</router-link>
+    <router-link to="/">Home</router-link>
+  </nav>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,52 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/brazil",
+    name: "Brazil",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Brazil.vue")
+  },
+  {
+    path: "/panama",
+    name: "Panama",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Panama.vue")
+  },
+  {
+    path: "/hawaii",
+    name: "Hawaii",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Hawaii.vue")
+  },
+  {
+    path: "/jamaica",
+    name: "Jamaica",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Jamaica.vue")
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
