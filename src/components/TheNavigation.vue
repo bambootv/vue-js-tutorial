bf5dd9ba3ccdba820ecbba12fab93d7cad057486
Hoan Ky <hoan.nguyen@luxstay.com>
2020-02-17 10:40:48 +0700

    Chapter 4

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,29 +1,64 @@
 <template>
   <nav id="nav">
-    <router-link to="/">Home</router-link>
-    <router-link to="/brazil">Brazil</router-link>
-    <router-link to="/panama">Panama</router-link>
-    <router-link to="/hawaii">Hawaii</router-link>
-    <router-link to="/jamaica">Jamaica</router-link>
-    <router-link to="/about">About</router-link>
+    <p class="logo">The Vue School travel app</p>
+    <ul class="nav-links">
+      <li class="links">
+        <router-link to="/">
+          Home
+        </router-link>
+      </li>
+      <li v-for="destination in destinations" :key="destination.name" class="links">
+        <router-link :to="{name: 'DestinationDetails', params: {id: destination.id}}" >
+          {{destination.name}}
+        </router-link>
+      </li>
+    </ul>
   </nav>
 </template>
+
+<script>
+import store from "@/assets/store"
+export default {
+  data(){
+    return {
+      destinationId: this.$route.params.id,
+      destinations: store.destinations
+    }
+  }
+}
+</script>
 
 <style scoped>
 #nav {
   display: flex;
-  padding: 30px;
-  justify-content: center;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
-  padding: 0 10px;
+  text-decoration: none;
+  font-weight: bold;
 }
 
 #nav a.vue-school-active-class {
   color: #42b983;
 }
 
+.nav-links {
+  display: flex;
+}
+
+.links {
+  padding-right: 20px;
+  list-style: none;
+}
+
+.links:hover {
+  text-decoration: underline;
+}
+
+.logo {
+  font-size: 20px;
+  color: purple;
+  font-weight: bold;
+}
 </style>
